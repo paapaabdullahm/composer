b863355410fba4d26553864f4539a4d8a7ec00f8
Paapa Abdullah Morgan <paapaabdullahm@gmail.com>
2019-09-09 16:03:09 +0000

    Set location /tini

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -50,13 +50,13 @@ isCommand() {
 
 # check if the first argument passed in looks like a flag
 if [ "$(printf %c "$1")" = '-' ]; then
-  set -- /sbin/tini -- composer "$@"
+  set -- /tini -- composer "$@"
 # check if the first argument passed in is composer
 elif [ "$1" = 'composer' ]; then
-  set -- /sbin/tini -- "$@"
+  set -- /tini -- "$@"
 # check if the first argument passed in matches a known command
 elif isCommand "$1"; then
-  set -- /sbin/tini -- composer "$@"
+  set -- /tini -- composer "$@"
 fi
 
 exec "$@"
